f4d421f24d007778d06542fbc1e88ae19fcc7631
lanigaines <lanigaines@live.com>
2024-09-23 01:14:28 -0700

    adding files
    workflow

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    ##################################################
+    # Comlete your code here
+    ##################################################
+    number = int(input('Enter your input: '))
+
+    """
+    Make your code here
+    """
+
+    print(f'Range is {range}')
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return range
+
+
+if __name__ == '__main__':
+    main()
